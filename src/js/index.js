914ba90ba14680a572c060a680cd54e16a9d8043
Artem <skeleton.ubica@gmail.com>
2025-03-14 03:31:43 +0300

    ref 14.03--01

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ const openButton = document.querySelector('.header__menu')
 const closeButton = document.querySelector('.header__close')
 const mainElBlur = document.querySelector('.main')
 const headElBlur = document.querySelector('.header')
-
+let swiper
 
 openButton.addEventListener('click', () => {
   sidebar.classList.add('sidebar--open')
@@ -19,4 +19,54 @@ closeButton.addEventListener('click', () => {
   sidebar.classList.remove('sidebar--open')
   mainElBlur.style.setProperty('filter', 'blur(0)')
   headElBlur.style.setProperty('filter', 'blur(0)')
+})
+
+const initSwiper = () => {
+  if (window.innerWidth < 768 && !swiper) {
+    swiper = new Swiper('.swiper', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets'
+      },
+      navigation: {
+        nextEl: '.swiper-button-next'
+      },
+      slidesPerView: 'auto',
+      spaceBetween: 15
+    })
+  } else if (window.innerWidth >= 768 && swiper) {
+    swiper.destroy(true, true)
+    swiper = undefined
+  }
+}
+
+window.addEventListener('load', initSwiper)
+window.addEventListener('resize', initSwiper)
+
+if (window.innerWidth >= 1120) {
+  const hiddenSlide = document.querySelectorAll('.hidden__slide')
+  hiddenSlide.forEach((value) => {
+    value.classList.add('swiper-slide')
+  })
+}
+
+const btnShowMore = document.querySelector('.show-more__button')
+const arrowShowMore = document.querySelector('.show-more__arrow')
+const sliderContainer = document.querySelector('.slider')
+const stateShowMore = { show: false }
+
+btnShowMore.addEventListener('click', () => {
+  if (!stateShowMore.show) {
+    sliderContainer.classList.add('show-more__slider')
+    arrowShowMore.classList.add('arrow__top')
+    btnShowMore.textContent = 'Скрыть'
+    stateShowMore.show = true
+  } else {
+    sliderContainer.classList.remove('show-more__slider')
+    arrowShowMore.classList.remove('arrow__top')
+    btnShowMore.textContent = 'Показать все'
+    stateShowMore.show = false
+  }
 })
